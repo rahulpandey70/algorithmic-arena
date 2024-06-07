afbb0f9e4e8efa4c0ac587d05d056e8390f9d44b
rahulpandey70 <rahulpandey9103@gmail.com>
2024-06-08 02:14:03 +0530

    fixes auth login issue

--- apps/web/app/lib/auth.ts
+++ apps/web/app/lib/auth.ts
@@ -7,121 +7,123 @@ import { JWTPayload, SignJWT, importJWK } from "jose";
 import { Session } from "next-auth";
 
 interface token extends JWT {
-  uid: string;
-  jwtToken: string;
+	uid: string;
+	jwtToken: string;
 }
 
 export interface session extends Session {
-  user: {
-    id: string;
-    jwtToken: string;
-    email: string;
-    name: string;
-  };
+	user: {
+		id: string;
+		jwtToken: string;
+		email: string;
+		name: string;
+	};
 }
 interface User {
-  id: string;
-  name: string;
-  email: string;
-  token: string;
+	id: string;
+	name: string;
+	email: string;
+	token: string;
 }
 
 const generateJWT = async (payload: JWTPayload) => {
-  const secret = process.env.JWT_SECRET || "secret";
+	const secret = process.env.JWT_SECRET || "secret";
 
-  const jwk = await importJWK({ k: secret, alg: "HS256", kty: "oct" });
+	const jwk = await importJWK({ k: secret, alg: "HS256", kty: "oct" });
 
-  const jwt = await new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
-    .setExpirationTime("365d")
-    .sign(jwk);
+	const jwt = await new SignJWT(payload)
+		.setProtectedHeader({ alg: "HS256" })
+		.setIssuedAt()
+		.setExpirationTime("365d")
+		.sign(jwk);
 
-  return jwt;
+	return jwt;
 };
 
 export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "email", type: "text", placeholder: "" },
-        password: { label: "password", type: "password", placeholder: "" },
-      },
-      async authorize(credentials: any) {
-        const hashedPassword = await bcrypt.hash(credentials.password, 10);
+	providers: [
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				username: { label: "email", type: "text", placeholder: "" },
+				password: { label: "password", type: "password", placeholder: "" },
+			},
+			async authorize(credentials: any) {
+				if (!credentials?.username || !credentials?.password) return null;
 
-        const userDb = await db.user.findFirst({
-          where: {
-            email: credentials.username,
-          },
-          select: {
-            password: true,
-            id: true,
-            name: true,
-          },
-        });
+				const hashedPassword = await bcrypt.hash(credentials.password, 10);
 
-        if (userDb) {
-          if (await bcrypt.compare(credentials.password, userDb.password)) {
-            const jwt = await generateJWT({
-              id: userDb.id,
-            });
+				const userDb = await db.user.findFirst({
+					where: {
+						email: credentials.username,
+					},
+					select: {
+						password: true,
+						id: true,
+						name: true,
+					},
+				});
 
-            return {
-              id: userDb.id,
-              name: userDb.name,
-              email: credentials.username,
-              token: jwt,
-            };
-          } else {
-            return null;
-          }
-        }
-        try {
-          // sign up
-          const user = await db.user.create({
-            data: {
-              email: credentials.username,
-              name: credentials.username,
-              password: hashedPassword,
-            },
-          });
+				if (userDb) {
+					if (await bcrypt.compare(credentials.password, userDb.password)) {
+						const jwt = await generateJWT({
+							id: userDb.id,
+						});
 
-          const jwt = await generateJWT({
-            id: user.id,
-          });
+						return {
+							id: userDb.id,
+							name: userDb.name,
+							email: credentials.username,
+							token: jwt,
+						};
+					} else {
+						return null;
+					}
+				}
+				try {
+					// sign up
+					const user = await db.user.create({
+						data: {
+							email: credentials.username,
+							name: credentials.username,
+							password: hashedPassword,
+						},
+					});
 
-          return {
-            id: user.id,
-            name: credentials.username,
-            email: credentials.username,
-            token: jwt,
-          };
-        } catch (e) {
-          return null;
-        }
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET || "secr3t",
-  callbacks: {
-    session: async ({ session, token }) => {
-      const newSession: session = session as session;
-      if (newSession.user && token.uid) {
-        newSession.user.id = token.uid as string;
-        newSession.user.jwtToken = token.jwtToken as string;
-      }
-      return newSession!;
-    },
-    jwt: async ({ token, user }): Promise<JWT> => {
-      const newToken = token;
+					const jwt = await generateJWT({
+						id: user.id,
+					});
 
-      if (user) {
-        newToken.uid = user.id;
-        newToken.jwtToken = (user as User).token;
-      }
-      return newToken;
-    },
-  },
+					return {
+						id: user.id,
+						name: credentials.username,
+						email: credentials.username,
+						token: jwt,
+					};
+				} catch (e) {
+					return null;
+				}
+			},
+		}),
+	],
+	secret: process.env.NEXTAUTH_SECRET || "secr3t",
+	callbacks: {
+		session: async ({ session, token }) => {
+			const newSession: session = session as session;
+			if (newSession.user && token.uid) {
+				newSession.user.id = token.uid as string;
+				newSession.user.jwtToken = token.jwtToken as string;
+			}
+			return newSession!;
+		},
+		jwt: async ({ token, user }): Promise<JWT> => {
+			const newToken = token;
+
+			if (user) {
+				newToken.uid = user.id;
+				newToken.jwtToken = (user as User).token;
+			}
+			return newToken;
+		},
+	},
 } satisfies NextAuthOptions;
